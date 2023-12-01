06f586012d723e7a35e477005139dee29fc8c2b0
Yuremeshi <yuremeshi.code@gmail.com>
2023-12-01 16:17:29 -0500

    Updates to events

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import CheckIcon from '../svg/checkmarkSVG';
+import XmarkIcon from '../svg/xmarkSVG';
 
-const event1 = {"guid": 1, "name": "Picnic", "location": "Park", "description": "Awesome Picnic", "image": "https://media-cdn.tripadvisor.com/media/photo-s/02/65/7a/27/filename-temple-of-love.jpg", "startTime": "June 6, 2024", "created": "May 20, 2024", "modified": "May 20, 2024", "deleted": "false"};
+const event1 = {"guid": 1, "name": "Church Picnic", "location": "Larz Anderson Park", "description": "Awesome Picnic", "image": "https://media-cdn.tripadvisor.com/media/photo-s/02/65/7a/27/filename-temple-of-love.jpg", "startTime": {"year": "2024", "month": "May", "day": "6"}, "created": "May 20, 2024", "modified": "May 20, 2024", "deleted": "false"};
 
-const event2 = {"guid": 2, "name": "Six Flags", "location": "Six Flags Theme Park", "description": "Six Flags Trip", "image": "https://upload.wikimedia.org/wikipedia/commons/9/9c/VR_Coaster_Train_at_Six_Flags_New_England.jpg", "startTime": "June 24, 2024", "created": "April 20, 2024", "modified": "April 20, 2024", "deleted": "false"};
+const event2 = {"guid": 2, "name": "Six Flags Trip", "location": "Six Flags New England", "description": "A trip to Six Flags", "image": "https://upload.wikimedia.org/wikipedia/commons/9/9c/VR_Coaster_Train_at_Six_Flags_New_England.jpg", "startTime": {"year": "2024", "month": "June", "day": "24"}, "created": "April 20, 2024", "modified": "April 20, 2024", "deleted": "false"};
 
 const events = [event1, event2];
 
@@ -11,21 +13,35 @@ export const Events = () => {
         <div className="flex flex-grow flex-col items-center max-w-[1200px] bg-wood-pattern border-x-[1px] border-x-amber-800 min-h-100% h-fit">
             <div className="flex flex-col items-center pt-40 w-full gap-10">
                 {events && (events.length > 0) && events.map((event, index) =>
-                    <div className="flex flex-grow max-w-[1100px] w-full h-[180px] bg-paper-pattern rounded-sm">
-                        <div className="flex items-end">
-                            { event.startTime }
-                        </div>
-                        <div>
-                            { event.name }
-                        </div>
-                        <div>
-                            { event.location }
-                        </div>
-                        <div>
-                            { event.description}
-                        </div>
-                        <div className="flex flex-grow justify-end">
+                    <div className="flex flex-grow max-w-[1100px] w-full h-[180px] bg-paper-pattern rounded-sm gap-4">
+                        <div className="flex">
                             <img className="h-full w-64 opacity-60 hover:opacity-100 hover:cursor-pointer" src={ event.image } />
+                        </div>
+                        <div className="flex flex-col justify-end mb-2">
+                            <div className="text-xl">
+                                { event.startTime.year }
+                            </div>
+                            <div className="text-7xl">
+                                { event.startTime.day }
+                            </div>
+                            <div className="text-4xl">
+                                { event.startTime.month }
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-end mb-5 ml-2">
+                            <div className="text-4xl">
+                                { event.name }
+                            </div>
+                            <div>
+                                Location: { event.location }
+                            </div>
+                            <div>
+                                Description: { event.description}
+                            </div>
+                        </div>
+                        <div className="flex flex-grow justify-end items-end mr-4 mb-4 gap-2">
+                            <button className="border-2 bg-sky-200 p-2 rounded-lg"><CheckIcon /></button>
+                            <button className="border-2 bg-sky-200 p-2 rounded-lg"><XmarkIcon /></button>
                         </div>
                     </div>
                 )}
