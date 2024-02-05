6267b57df1da15ba0be608e8dbccd70720830014
Yuremeshi <yuremeshi.code@gmail.com>
2024-02-05 15:22:21 -0500

    Unfinished visx chart

---
+++ src/components/misc/Chart.tsx
@@ -0,0 +1,67 @@
+import React, { useMemo } from 'react';
+import { Bar } from '@visx/shape';
+import { Group } from '@visx/group';
+import { scaleBand, scaleLinear } from '@visx/scale';
+
+const verticalMargin = 10;
+
+export type BarsProps = {
+    width: number;
+    height: number;
+    events?: Boolean;
+    going: number;
+    notgoing: number;
+};
+
+export default function Chart({ width, height, events = false, going, notgoing }: BarsProps) {
+    const xMax = width;
+    const yMax = height - verticalMargin;
+
+    const xScale = useMemo(
+        () =>
+          scaleBand<string>({
+            range: [0, xMax],
+            round: true,
+            domain: ["Going", "Not Going"],
+            padding: 0.4,
+          }),
+        [xMax],
+      );
+      const yScale = useMemo(
+        () =>
+          scaleLinear<number>({
+            range: [yMax, 0],
+            round: true,
+            domain: [0, Math.max(going, notgoing)],
+          }),
+        [yMax],
+      );
+    
+      return width < 10 ? null : (
+        <svg width={width} height={height}>
+          <rect width={width} height={height} fill="url(#teal)" rx={14} />
+          <Group top={verticalMargin / 2}>
+            {data.map((d) => {
+              const letter = getLetter(d);
+              const barWidth = xScale.bandwidth();
+              const barHeight = yMax - (yScale(getLetterFrequency(d)) ?? 0);
+              const barX = xScale(letter);
+              const barY = yMax - barHeight;
+              return (
+                <Bar
+                  key={`bar-${letter}`}
+                  x={barX}
+                  y={barY}
+                  width={barWidth}
+                  height={barHeight}
+                  fill="rgba(23, 233, 217, .5)"
+                  onClick={() => {
+                    if (events) alert(`clicked: ${JSON.stringify(Object.values(d))}`);
+                  }}
+                />
+              );
+            })}
+          </Group>
+        </svg>
+      );
+}
